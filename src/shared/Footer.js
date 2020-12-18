3b7a38cfaab531a7ebb488119633dfd8d6b17db3
Nicolas Montanez <nicomontanez98@gmail.com>
2020-12-17 19:43:38 -0500

    Se agregó el formulario de ingreso y de registro

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <footer class="footer mt-auto py-3 bg-dark">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  );
 }
